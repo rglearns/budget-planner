92d8abffa3c1763458fd5e788718de5bb9c8935a
Rajat Gupta <rajat.gupta@amadeus.com>
2019-07-06 20:26:44 +0530

    initial commit

---
+++ src/app/expenses.model.ts
@@ -0,0 +1,8 @@
+export class Expenses {
+    reason: string;
+    amount: number;
+    constructor(reason: string, amount: number) {
+     this.amount = amount;
+     this.reason = reason;
+    }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Expenses } from '../expenses.model';
+import { alertController } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  title: string;
+  reasonText: string;
+  amountText: string;
+  clearText: string;
+  addText: string;
+  enteredReason: string;
+  enteredAmount: number;
+
+  expenses: Expenses[] = [];
 
   constructor() {}
+  ngOnInit() {
+    this.title = 'Budget Planner';
+    this.reasonText = 'Enter expense';
+    this.amountText = 'Enter Amount';
+    this.clearText = 'Clear';
+    this.addText = 'Add';
+  }
+  clear() {
+    this.enteredAmount = null;
+    this.enteredReason = '';
+  }
+  saveExpense() {
+    if (this.enteredAmount && this.enteredReason && this.enteredAmount > 0) {
+    const expense = new Expenses(this.enteredReason, this.enteredAmount);
+    this.expenses.push(expense);
+    } else {
+      alertController.create({
+        message: 'Please Enter Valid Reason and amount',
+        header: 'Error',
+        buttons:[{
+          role: 'cancel',
+          text: 'OK'
+        }]
+      }).then(alert => {
+        alert.present();
+      });
+    }
+  }
 
 }
